0d8c47b9008021fae703eefb4ce51ad0a7718777
Josef <josef.tourrand@epita.fr>
2022-10-02 21:32:43 +0200

    build: created neutralize.h in order to be able to reuse load_image

---
+++ src/preproc/main.c
@@ -0,0 +1,24 @@
+#include <err.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "neutralize.h"
+
+int main(int argc, char** argv)
+{
+    if (argc != 3)
+        errx(EXIT_FAILURE, "Usage: neutralize <image-file> <outfile>");
+
+    // - Create a surface from the image.
+    SDL_Surface* surface = load_image(argv[1]);
+
+    // - Neutralizes the image and saves it.
+    surface_to_grayscale(surface);
+    char* filename = argv[2];
+    IMG_SaveJPG(surface,filename,100);
+    
+    // - Cleanup
+    SDL_FreeSurface(surface);
+    SDL_Quit();
+    
+    return EXIT_SUCCESS;
+}
