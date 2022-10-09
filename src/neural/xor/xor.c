94be4065747a3f84e422ed389917ccbba83692a6
Amol <adresse.site.comptes@gmail.com>
2022-10-09 11:06:25 +0100

    feat: filter and process main input

--- src/neural/xor/xor.c
+++ src/neural/xor/xor.c
@@ -1,9 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <err.h>
 #include "xor.h"
 
 int main(int argc, char** argv)
 {
-  learn(atoi(argv[1]));
+  if (argc < 2)
+    errx(EXIT_FAILURE, "missing parameters");
+
+  if (strcmp(argv[1], "-l") == 0)
+  {
+    if (argc == 2)
+      learn(10000);
+    else if (argc == 3)
+      learn(atoi(argv[2]));
+    else
+      errx(EXIT_FAILURE, "too many parameters");
+  }
+  else if (strcmp(argv[1], "-u") == 0)
+  {
+    // TODO
+  }
+  else
+    errx(EXIT_FAILURE, "unknown parameter %s", argv[1]);
+
   return 0;
 }
