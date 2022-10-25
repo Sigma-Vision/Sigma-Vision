37f99476b60cb586678a6a5f95d1cce0696588ee
Josef <jtourrand@gmail.com>
2022-10-25 19:46:49 +0200

    help: plz checkout on this commit and check if segfault when compiling

--- src/preproc/tools.h
+++ src/preproc/tools.h
@@ -3,7 +3,9 @@
 #ifndef TOOLS_H
 #define TOOLS_H
 
+Uint8 GetColor(SDL_Surface* surface, int i, int j);
 SDL_Surface* load_image(const char* path);
 SDL_Surface* GaussianBlur (SDL_Surface* surface, int radius);
+SDL_Surface* SobelTransform(SDL_Surface* surface);
 
 #endif
