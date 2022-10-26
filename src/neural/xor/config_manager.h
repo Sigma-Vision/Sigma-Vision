faddfee8f03136327650ca31b0de72a22a42ca60
malo.garnier <malo.garnier@epita.fr>
2022-10-26 15:05:40 +0200

    feat: get weights and biases full config

--- src/neural/xor/config_manager.h
+++ src/neural/xor/config_manager.h
@@ -8,3 +8,6 @@ void write_config(const int nb_inputs, const int nb_layers,
 int get_nb_layers();
 
 void get_nb_nodes(const int nb_layers, int* nb_nodes_p);
+
+void get_config(const int nb_inputs, const int nb_layers, const int* nb_nodes_p,
+                int** biases_pp, int*** weights_ppp);
