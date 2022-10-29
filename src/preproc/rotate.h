816dc00d5742c066331ea87eb05e3db4ea6c8f13
Josef <jtourrand@gmail.com>
2022-10-29 23:24:10 +0200

    fix: RotateDetectedGrid

--- src/preproc/rotate.h
+++ src/preproc/rotate.h
@@ -1,4 +1,5 @@
 #include <SDL2/SDL_image.h>
+#include "struct.h"
 
 #ifndef ROTATE_H
 #define ROTATE_H
@@ -7,5 +8,6 @@ SDL_Surface* rotateAny(SDL_Surface* surface, double angle,int color_fill);
 void rotate180(SDL_Surface* surface);
 SDL_Surface* rotate90ccw(SDL_Surface* surface);
 SDL_Surface* rotate90cw(SDL_Surface* surface);
+SDL_Surface* RotateDetectedGrid(SDL_Surface* surface,Dot* dot1, Dot* dot2);
 
 #endif
