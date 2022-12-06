5330c1f74cad55177c039629e7821da2def50ceb
paul Dufour <paul.dufour@epita.fr>
2022-12-06 14:12:46 +0100

    update preproc to merge qith ui
    Signed-off-by: paul Dufour <paul.dufour@epita.fr>

--- src/preproc/preproc.c
+++ src/preproc/preproc.c
@@ -9,14 +9,8 @@
 #include "scale.h"
 #include "transform.h"
 
-int preproc(char* file, char* output)
+SDL_Surface* preproc(SDL_Surface* surface)
 {
-    if (file == NULL)
-        errx(EXIT_FAILURE, "Usage: neutralize <image-file> <outfile>");
-
-    // - Create a surface from the image.
-    SDL_Surface* surface = load_image(file);
-
     // - Neutralizes the image and saves it.
     surface_to_grayscale(surface);
 
@@ -31,24 +25,20 @@ int preproc(char* file, char* output)
     Square s;
     find_grid(surface, &s);
   
-    
 
     //rotate180(surface);
 
 //  surface = rotateAny(surface,0,255,1);
     
     surface = RotateDetectedGrid(surface,&s); 
-
+/*
     SDL_Surface* to_free = surface;
     surface = GridCropping(to_free,&s);
     SDL_FreeSurface(to_free);
 
     GridSplit(surface);
+*/
 
-    IMG_SaveJPG(surface, output, 100);
-    // - Cleanup
-    SDL_FreeSurface(surface);
-    SDL_Quit();
     
-    return EXIT_SUCCESS;
+    return surface;
 }
